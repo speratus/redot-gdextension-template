b13fb52e40c297acbdb68be144c13ae040d867be
Andrew Luchuk <andrew.luchuk@outlook.com>
2026-03-13 23:58:40 -0400

    Scaffold GDExtension template

---
+++ src/register_types.cpp
@@ -0,0 +1,40 @@
+//
+// Created by andrew on 3/13/26.
+//
+
+#include "register_types.h"
+
+#include <gdextension_interface.h>
+
+using namespace godot;
+
+void register_gdextension_types(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+
+    //TODO: Implement type registration
+}
+
+void unregister_gdextension_types(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+
+    //TODO: Implement plugin unregistration
+}
+
+extern "C" {
+GDExtensionBool GDE_EXPORT init_gdextension(
+    GDExtensionInterfaceGetProcAddress p_get_proc_address,
+    const GDExtensionClassLibraryPtr p_library,
+    GDExtensionInitialization *r_initialization
+    ) {
+    godot::GDExtensionBinding::InitObject init_object(p_get_proc_address, p_library, r_initialization);
+
+    init_object.register_initializer(register_gdextension_types);
+    init_object.register_terminator(unregister_gdextension_types);
+    init_object.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_LATE);
+}
+}
+
---
+++ src/register_types.h
@@ -0,0 +1,13 @@
+//
+// Created by andrew on 3/13/26.
+//
+
+#ifndef GDEXTENSION_TEMPLATE_REGISTER_TYPES_H
+#define GDEXTENSION_TEMPLATE_REGISTER_TYPES_H
+
+using namespace godot;
+
+void register_gdextension_types();
+void unregister_gdextension_types();
+
+#endif //GDEXTENSION_TEMPLATE_REGISTER_TYPES_H
